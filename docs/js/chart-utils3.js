ba2f240df0170087165f9eba9174f2391d2cd516
goldenstateofmind <jhnklly@gmail.com>
2022-12-06 18:08:00 -0800

    Tidy; explain

--- docs/js/chart-utils3.js
+++ docs/js/chart-utils3.js
@@ -8,18 +8,12 @@
   TODO: https://bl.ocks.org/larsenmtl/e3b8b7c2ca4787f77d78f58d41c3da91
 */
 
-var margin = { top: 20, right: 80, bottom: 30, left: 20 },
+var margin = {top: 20, right: 80, bottom: 30, left: 20},
   width = 1500 - margin.left - margin.right,
   height = 1000 - margin.top - margin.bottom
 
-var xScaleLinear = d3
-  .scaleLinear()
-  .range([0, width])
-  .nice()
-var yScaleLinear = d3
-  .scaleLinear()
-  .range([height, 0])
-  .nice()
+var xScaleLinear = d3.scaleLinear().range([0, width]).nice()
+var yScaleLinear = d3.scaleLinear().range([height, 0]).nice()
 
 // xScaleBand = d3.scaleBand().rangeRound([0, width]).padding(.65);
 // xScaleBand = d3.scaleBand().range([0, width]).padding(0.5);
@@ -41,13 +35,13 @@ spectral9 = [
   '#e6f598',
   '#abdda4',
   '#66c2a5',
-  '#3288bd'
+  '#3288bd',
 ]
 
 var area = d3
   .area()
   .x((d, di) => Math.round(xScaleLinear(di)))
-  .y1(d => Math.round(yScaleLinear(d)))
+  .y1((d) => Math.round(yScaleLinear(d)))
 // Prep the toltp bits, initial display is hidden
 var toltp
 
@@ -55,7 +49,7 @@ function initChart(bandCount) {
   console.log('initChart')
 
   var whichMonthsCharted = [...Array(bandCount).keys()].map(
-    x => 12 * (x + 1) - 1
+    (x) => 12 * (x + 1) - 1
   )
   xScaleBand.domain(whichMonthsCharted)
 
@@ -77,25 +71,13 @@ function initChart(bandCount) {
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
   svg.append('g').attr('id', 'legend')
 
-  svgG
-    .append('path')
-    .attr('id', 'data-a-area')
-    .attr('class', 'area')
+  svgG.append('path').attr('id', 'data-a-area').attr('class', 'area')
 
-  svgG
-    .append('path')
-    .attr('id', 'data-b-area')
-    .attr('class', 'area')
+  svgG.append('path').attr('id', 'data-b-area').attr('class', 'area')
 
-  svgG
-    .append('path')
-    .attr('id', 'data-a-line')
-    .attr('class', 'line')
+  svgG.append('path').attr('id', 'data-a-line').attr('class', 'line')
 
-  svgG
-    .append('path')
-    .attr('id', 'data-b-line')
-    .attr('class', 'line')
+  svgG.append('path').attr('id', 'data-b-line').attr('class', 'line')
 
   svg
     .append('g')
@@ -112,11 +94,10 @@ function initChart(bandCount) {
     .attr('id', 'data-b-stacked-bars')
     .attr('class', 'stacked')
     // .attr("transform", `translate(${margin.left + 17}, ${margin.top})`)
-    .attr('transform', d => {
-      return `translate(${margin.left +
-        indent +
-        A.bandwidthFraction * xScaleBand.bandwidth() +
-        4}, ${margin.top})`
+    .attr('transform', (d) => {
+      return `translate(${
+        margin.left + indent + A.bandwidthFraction * xScaleBand.bandwidth() + 4
+      }, ${margin.top})`
     })
 
   // Add the axes
@@ -150,26 +131,20 @@ function initChart(bandCount) {
     .data(['chart-bar', 'chart-line'])
     .enter()
     .append('a')
-    .attr('id', d => `a-${d}`)
-    .on('mouseover', d => {
+    .attr('id', (d) => `a-${d}`)
+    .on('mouseover', (d) => {
       console.log(d)
       if (d === 'chart-bar') {
         d3.selectAll('#data-a-stacked-bars,#data-b-stacked-bars')
           .transition()
           .duration(400)
           .style('opacity', 1)
-        d3.select('#legend')
-          .transition()
-          .duration(400)
-          .style('opacity', 1)
+        d3.select('#legend').transition().duration(400).style('opacity', 1)
         d3.select('#hmh-g-area-line-chart')
           .transition()
           .duration(400)
           .style('opacity', 0)
-        d3.select('#toltp')
-          .transition()
-          .duration(400)
-          .style('opacity', 1)
+        d3.select('#toltp').transition().duration(400).style('opacity', 1)
         d3.select('#a-chart-bar').classed('active', true)
         d3.select('#a-chart-line').classed('active', false)
       } else {
@@ -177,37 +152,27 @@ function initChart(bandCount) {
           .transition()
           .duration(400)
           .style('opacity', 0)
-        d3.select('#legend')
-          .transition()
-          .duration(400)
-          .style('opacity', 0)
+        d3.select('#legend').transition().duration(400).style('opacity', 0)
         d3.select('#hmh-g-area-line-chart')
           .transition()
           .duration(400)
           .style('opacity', 1)
-        d3.select('#toltp')
-          .transition()
-          .duration(400)
-          .style('opacity', 0)
+        d3.select('#toltp').transition().duration(400).style('opacity', 0)
         d3.select('#a-chart-bar').classed('active', false)
         d3.select('#a-chart-line').classed('active', true)
       }
     })
     .attr('class', 'toggle-chart')
-    .append('i')
-    .attr('class', d => `fas fa-fw fa-${d}`)
+    .append('span')
+    .attr('class', 'inline-flex m-1 underline')
+    .html((d) => (d === 'chart-bar' ? 'bars' : 'lines'))
 }
 
 function updateChartArea(aData, aData_hi, aData_lo, bData, bData_hi, bData_lo) {
   console.log('updateChartArea')
 
   var xMax = Math.max(aData.length, bData.length)
-  var yMax = Math.max(
-    ...aData
-      .concat(bData)
-      .concat(aData_hi)
-      .concat(bData_hi)
-  )
+  var yMax = Math.max(...aData.concat(bData).concat(aData_hi).concat(bData_hi))
 
   // Scale the range of the data
   xScaleLinear.domain([0, xMax])
@@ -217,15 +182,15 @@ function updateChartArea(aData, aData_hi, aData_lo, bData, bData_hi, bData_lo) {
   var valueline = d3
     .line()
     .x((d, di) => xScaleLinear(di))
-    .y(d => yScaleLinear(d))
+    .y((d) => yScaleLinear(d))
   a_hiLoData = []
   aData_hi.forEach((x, xi) => {
-    a_hiLoData.push({ hi: x, lo: aData_lo[xi] })
+    a_hiLoData.push({hi: x, lo: aData_lo[xi]})
   })
 
   b_hiLoData = []
   bData_hi.forEach((x, xi) => {
-    b_hiLoData.push({ hi: x, lo: bData_lo[xi] })
+    b_hiLoData.push({hi: x, lo: bData_lo[xi]})
   })
 
   var svgG = d3.select('#hmh-g-area-line-chart')
@@ -239,11 +204,11 @@ function updateChartArea(aData, aData_hi, aData_lo, bData, bData_hi, bData_lo) {
       'd',
       d3
         .area()
-        .x(function(d, di) {
+        .x(function (d, di) {
           return xScaleLinear(di)
         })
-        .y0(d => yScaleLinear(d.lo))
-        .y1(d => yScaleLinear(d.hi))
+        .y0((d) => yScaleLinear(d.lo))
+        .y1((d) => yScaleLinear(d.hi))
     )
   // .exit()
   // .remove()
@@ -257,11 +222,11 @@ function updateChartArea(aData, aData_hi, aData_lo, bData, bData_hi, bData_lo) {
       'd',
       d3
         .area()
-        .x(function(d, di) {
+        .x(function (d, di) {
           return xScaleLinear(di)
         })
-        .y0(d => yScaleLinear(d.lo))
-        .y1(d => yScaleLinear(d.hi))
+        .y0((d) => yScaleLinear(d.lo))
+        .y1((d) => yScaleLinear(d.hi))
     )
 
   // Add the valueline path.
@@ -285,7 +250,7 @@ function updateChartArea(aData, aData_hi, aData_lo, bData, bData_hi, bData_lo) {
   d3.select('#y-r-axis').call(d3.axisRight(yScaleLinear))
 } // updateChartArea
 
-createLegend = svg => {
+createLegend = (svg) => {
   // const g = svg
 
   svg.select('#legend').html('') // hack?
@@ -298,29 +263,24 @@ createLegend = svg => {
     .attr('font-size', 13)
     .selectAll('g')
     // .data( Object.values(A.colorLookup).slice().reverse())
-    .data(
-      Object.values(A.colorLookup)
-        .slice()
-        .reverse(),
-      d => {
-        // return d;
-        // return d.key; // undefined
-        return 1
-      }
-    )
+    .data(Object.values(A.colorLookup).slice().reverse(), (d) => {
+      // return d;
+      // return d.key; // undefined
+      return 1
+    })
     .join('g')
     .attr('transform', (d, i) => `translate(0,${i * 20})`)
   g.append('rect')
     .attr('x', -19)
     .attr('width', 19)
     .attr('height', 19)
-    .attr('fill', d => d)
+    .attr('fill', (d) => d)
 
   g.append('text')
     .attr('x', 9)
     .attr('y', 9.5)
     .attr('dy', '0.35em')
-    .text(d => parseKey(A.keynameLookup[d]))
+    .text((d) => parseKey(A.keynameLookup[d]))
 }
 
 grays = ['#fefefe', '#fdfdfd', '#fcfcfc', '#fbfbfb', '#fafafa']
@@ -335,7 +295,7 @@ A.colorLookup = {
   neg_Loan_Interest: grays[0],
   neg_Property_Tax: grays[1],
   neg_Insurance: grays[2],
-  'neg_Maintenance_/_Improvements': grays[3]
+  'neg_Maintenance_/_Improvements': grays[3],
 }
 
 // A.colorLookup["pos_First_Deposit_+_Growth"] = "#FDA460";
@@ -359,12 +319,12 @@ function updateChart2(aData, bData) {
     'neg_Loan_Interest',
     'neg_Property_Tax',
     'neg_Insurance',
-    'neg_Maintenance_/_Improvements'
+    'neg_Maintenance_/_Improvements',
   ]
   bKeys = [
     'pos_First_Deposit_+_Growth',
     'pos_Recurring_Deposits_+_Growth',
-    'neg_Rent'
+    'neg_Rent',
   ]
 
   A.aKeys = aKeys // ["monthsCompleted", "yearsCompleted", "misc_newHouseValue", "pos_Equity_From_Principal", "pos_Equity_From_Appreciation", "pos_Tax_Savings", "neg_Loan_Interest", "neg_Property_Tax", "neg_Insurance", "neg_Maintenance"]
@@ -381,7 +341,7 @@ function updateChart2(aData, bData) {
   // xScaleBand.domain([0, A.yearCount]);
   // xScaleBand.domain(arrN);
   xScaleBand.domain(
-    aData.map(function(d) {
+    aData.map(function (d) {
       return d.monthsCompleted
     })
   )
@@ -412,9 +372,9 @@ function updateChart2(aData, bData) {
     .append('g')
     .merge(barSections)
     // .attr("class", "g-stack")
-    .attr('data-category', d => d.key)
+    .attr('data-category', (d) => d.key)
     .attr('fill-opacity', 0.97)
-    .attr('class', function(d) {
+    .attr('class', function (d) {
       prefix = d.key.split('_')[0]
       return prefix + ' ' + A.colorLookup[d.key].replace('#', '')
       // return A.colorLookup[d.key];
@@ -422,22 +382,24 @@ function updateChart2(aData, bData) {
     .selectAll('rect')
     // .data(function(d) { return d; })
     .data(
-      d => d,
-      d => d.key
+      (d) => d,
+      (d) => d.key
     )
+
+  const heightLinear = (d) => {
+    const diff = yScaleLinear(d[0]) - yScaleLinear(d[1])
+    return diff > 0 ? diff : 0
+  }
+
   // enter and append the rects
   const enterBars = bars
     .enter()
     .append('rect')
-    .attr('x', function(d) {
+    .attr('x', function (d) {
       return xScaleBand(d.data.monthsCompleted)
     })
-    .attr('y', function(d) {
-      return yScaleLinear(d[1])
-    })
-    .attr('height', function(d) {
-      return yScaleLinear(d[0]) - yScaleLinear(d[1])
-    })
+    .attr('y', (d) => yScaleLinear(d[1]))
+    .attr('height', heightLinear)
     .attr('width', A.bandwidthFraction * xScaleBand.bandwidth())
   bars.exit().remove()
 
@@ -446,24 +408,20 @@ function updateChart2(aData, bData) {
     .transition()
     .duration(500)
     // .delay((d,i)=> i * 50)
-    .attr('x', function(d) {
+    .attr('x', function (d) {
       return xScaleBand(d.data.monthsCompleted)
     })
-    .attr('y', function(d) {
-      return yScaleLinear(d[1])
-    })
-    .attr('height', function(d) {
-      return yScaleLinear(d[0]) - yScaleLinear(d[1])
-    })
+    .attr('y', (d) => yScaleLinear(d[1]))
+    .attr('height', heightLinear)
     .attr('width', A.bandwidthFraction * xScaleBand.bandwidth())
 
   enterBars
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       if (d3.select('#a-chart-bar').classed('active')) {
         toltp.style('display', null)
       }
     })
-    .on('mouseout', function() {
+    .on('mouseout', function () {
       toltp.style('display', 'none')
     })
     .on('mousemove', moveTooltip)
@@ -490,9 +448,9 @@ function updateChart2(aData, bData) {
     .merge(barSectionsB)
     //.attr("class", "g-stack")
     // .attr("data-category", d => d.key.split('_')[1])
-    .attr('data-category', d => d.key)
+    .attr('data-category', (d) => d.key)
     .attr('fill-opacity', 0.87)
-    .attr('class', function(d) {
+    .attr('class', function (d) {
       prefix = d.key.split('_')[0]
       return prefix + ' ' + A.colorLookup[d.key].replace('#', '')
       // return A.colorLookup[d.key];
@@ -500,22 +458,18 @@ function updateChart2(aData, bData) {
     .selectAll('rect')
     // .data(function(d) { return d; })
     .data(
-      d => d,
-      d => d.key
+      (d) => d,
+      (d) => d.key
     )
   // enter and append the rects
   const enterBarsB = barsB
     .enter()
     .append('rect')
-    .attr('x', function(d) {
+    .attr('x', function (d) {
       return xScaleBand(d.data.monthsCompleted)
     })
-    .attr('y', function(d) {
-      return yScaleLinear(d[1])
-    })
-    .attr('height', function(d) {
-      return yScaleLinear(d[0]) - yScaleLinear(d[1])
-    })
+    .attr('y', (d) => yScaleLinear(d[1]))
+    .attr('height', heightLinear)
     .attr('width', A.bandwidthFraction * xScaleBand.bandwidth())
   barsB.exit().remove()
 
@@ -523,24 +477,20 @@ function updateChart2(aData, bData) {
     .merge(barsB)
     .transition()
     .duration(500)
-    .attr('x', function(d) {
+    .attr('x', function (d) {
       return xScaleBand(d.data.monthsCompleted)
     })
-    .attr('y', function(d) {
-      return yScaleLinear(d[1])
-    })
-    .attr('height', function(d) {
-      return yScaleLinear(d[0]) - yScaleLinear(d[1])
-    })
+    .attr('y', (d) => yScaleLinear(d[1]))
+    .attr('height', heightLinear)
     .attr('width', A.bandwidthFraction * xScaleBand.bandwidth())
 
   enterBarsB
-    .on('mouseover', function() {
+    .on('mouseover', function () {
       if (d3.select('#a-chart-bar').classed('active')) {
         toltp.style('display', null)
       }
     })
-    .on('mouseout', function() {
+    .on('mouseout', function () {
       toltp.style('display', 'none')
     })
     .on('mousemove', moveTooltip)
--- docs/js/config.js
+++ docs/js/config.js
@@ -1,13 +1,10 @@
-var A = {};
+var A = {}
 
+A.inputs = []
+A.yMax = 4500000
+A.compoundingPeriodsPerYear = 12
 
-A.inputs = [];
-A.yMax = 4500000;
-A.compoundingPeriodsPerYear = 12;
-
-numeral.defaultFormat('$0,0');
-
-
+numeral.defaultFormat('$0,0')
 
 /*
     Inputs    low high  Upfront $/Yr  $/Mo
@@ -29,160 +26,159 @@ numeral.defaultFormat('$0,0');
   15  Insurance 0.13% 0.13% 0.5   $1,260  $105
 */
 
-
 var inputRent = {
-  "id": "inputRent",
-  "label": "Current Rent",
-  "type": "text",
-  "class": "hmh-input param",
-  "divClass": "rent-component",
-  "step": 100,
-  "data-format": "$0,0",
-  "value": numeral(2200).format()
+  id: 'inputRent',
+  label: 'Current Rent',
+  type: 'text',
+  class: 'hmh-input param',
+  divClass: 'rent-component',
+  step: 100,
+  'data-format': '$0,0',
+  value: numeral(3500).format(),
 }
 var inputHousePrice = {
-  "id": "inputHousePrice",
-  "label": "House Price",
-  "type": "text",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "step": 50000,
-  "data-format": "$0,0",
-  "value": numeral(900000).format()
+  id: 'inputHousePrice',
+  label: 'House Price',
+  type: 'text',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  step: 50000,
+  'data-format': '$0,0',
+  value: numeral(1000000).format(),
 }
 var inputDownPaymentPercent = {
-  "id": "inputDownPaymentPercent",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Down Payment",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(20/100).format('0%')
+  id: 'inputDownPaymentPercent',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Down Payment',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(20 / 100).format('0%'),
 }
 
 var inputMortgageRate = {
-  "id": "inputMortgageRate",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Loan Rate",
-  "step": 0.001,
-  "type": "text",
-  "data-format": "0.0%",
-  "value": numeral(3.6/100).format('0.0%')
+  id: 'inputMortgageRate',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Loan Rate',
+  step: 0.001,
+  type: 'text',
+  'data-format': '0.0%',
+  value: numeral(6 / 100).format('0.0%'),
 }
 
 var inputMortgageYears = {
-  "id": "inputMortgageYears",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Loan term (yrs)",
-  "step": 0,
-  "type": "text",
-  "data-format": "0",
-  "value": 30
+  id: 'inputMortgageYears',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Loan term (yrs)',
+  step: 0,
+  type: 'text',
+  'data-format': '0',
+  value: 30,
 }
 
 var inputEstHouseGrowthRate = {
-  "id": "inputEstHouseGrowthRate",
-  "class": "hmh-input param",
-  "divClass": "estimate",
-  "label": "Estimate House Appreciation",
-  "step": 0.01,
-  "type": "text",
-  "check-uncertainty": true,
-  "data-format": "0%",
-  "value": numeral(3/100).format('0%')
+  id: 'inputEstHouseGrowthRate',
+  class: 'hmh-input param',
+  divClass: 'estimate',
+  label: 'Estimate House Appreciation',
+  step: 0.01,
+  type: 'text',
+  'check-uncertainty': true,
+  'data-format': '0%',
+  value: numeral(3 / 100).format('0%'),
 }
 
 var inputEstRentGrowthRate = {
-  "id": "inputEstRentGrowthRate",
-  "class": "hmh-input param",
-  "divClass": "estimate",
-  "label": "Estimate Rent Growth Rate",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(1/100).format('0%')
+  id: 'inputEstRentGrowthRate',
+  class: 'hmh-input param',
+  divClass: 'estimate',
+  label: 'Estimate Rent Growth Rate',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(1 / 100).format('0%'),
 }
 var inputEstIndexFundReturnRate = {
-  "id": "inputEstIndexFundReturnRate",
-  "class": "hmh-input param",
-  "divClass": "estimate",
-  "label": "Estimate Index Fund Return Rate",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "check-uncertainty": true,
-  "value": numeral(6/100).format('0%')
+  id: 'inputEstIndexFundReturnRate',
+  class: 'hmh-input param',
+  divClass: 'estimate',
+  label: 'Estimate Index Fund Return Rate',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  'check-uncertainty': true,
+  value: numeral(6 / 100).format('0%'),
 }
 var inputEstInflationRate = {
-  "id": "inputEstInflationRate",
-  "class": "hmh-input param",
-  "divClass": "estimate",
-  "label": "Estimate Inflation Rate",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(2/100).format('0%')
+  id: 'inputEstInflationRate',
+  class: 'hmh-input param',
+  divClass: 'estimate',
+  label: 'Estimate Inflation Rate',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(3.5 / 100).format('0%'),
 }
 
-var inputPropertyTaxRate = { // https://sftreasurer.org/property-taxes
+var inputPropertyTaxRate = {
+  // https://sftreasurer.org/property-taxes
   // https://www.sfassessor.org/sites/default/files/press-releases/Prop-13-Fact-sheet.pdf
   // increases by the inflation rate of the California Consumer Price Index (cap at 2%)
-  "id": "inputPropertyTaxRate",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Property Tax Rate",
-  "step": 0.001,
-  "type": "text",
-  "data-format": "0.00%",
-  "value": numeral(1.17/100).format('0.00%') // FY2018-2019 Property Tax Rate = 1.1630%
+  id: 'inputPropertyTaxRate',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Property Tax Rate',
+  step: 0.001,
+  type: 'text',
+  'data-format': '0.00%',
+  value: numeral(1.17 / 100).format('0.00%'), // FY2018-2019 Property Tax Rate = 1.1630%
 }
 var inputInsuranceRate = {
-  "id": "inputInsuranceRate",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Insurance Rate",
-  "step": 0.001,
-  "type": "text",
-  "data-format": "0.00%",
-  "value": numeral(0.13/100).format('0.00%')
+  id: 'inputInsuranceRate',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Insurance Rate',
+  step: 0.001,
+  type: 'text',
+  'data-format': '0.00%',
+  value: numeral(0.13 / 100).format('0.00%'),
 }
 
 var inputMarginalTaxRate = {
-  "id": "inputMarginalTaxRate",
-  "class": "hmh-input param",
-  "divClass": "estimate",
-  "label": "Marginal Tax Rate (Fed + State)",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(40/100).format('0%')
+  id: 'inputMarginalTaxRate',
+  class: 'hmh-input param',
+  divClass: 'estimate',
+  label: 'Marginal Tax Rate (Fed + State)',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(40 / 100).format('0%'),
 }
 
 var inputClosingCostRate = {
-  "id": "inputClosingCostRate",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Closing Cost",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(4/100).format('0%')
+  id: 'inputClosingCostRate',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Closing Cost',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(4 / 100).format('0%'),
 }
 
 var inputEstHomeImprovements = {
-  "id": "inputEstHomeImprovements",
-  "class": "hmh-input param",
-  "divClass": "house-component",
-  "label": "Home Improvements",
-  "step": 0.01,
-  "type": "text",
-  "data-format": "0%",
-  "value": numeral(1/100).format('0%')
+  id: 'inputEstHomeImprovements',
+  class: 'hmh-input param',
+  divClass: 'house-component',
+  label: 'Home Improvements',
+  step: 0.01,
+  type: 'text',
+  'data-format': '0%',
+  value: numeral(1 / 100).format('0%'),
 }
-
 
 A.inputs = [
   inputRent,
